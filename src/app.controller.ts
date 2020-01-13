0d72831aaa81ca6f55fcf74389d8f2b0b9ddbb16
Tran Tuan <tran.van.tuan@mqsolutions.com.vn>
2020-01-13 14:46:01 +0700

    init nestjs project

---
+++ src/app.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Get, Request, Post, UseGuards  } from '@nestjs/common';
+import { AppService } from './app.service';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth/auth.service';
+
+@Controller()
+export class AppController {
+  constructor(
+    private readonly appService: AppService,
+    private readonly authService: AuthService,
+  ) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @UseGuards(AuthGuard('local'))
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+  @UseGuards(AuthGuard('jwt'))
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+}
